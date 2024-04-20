0749a0b2ad8065fe322dffce4714ee40f81cdc0b
Abdul Rahman Al Alawi <99887416+alalawi-a-m@users.noreply.github.com>
2024-04-20 11:25:20 +0400

    My first comment

---
+++ src/githubtesting/GithubTesting.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubtesting;
+
+/**
+ *
+ * @author RAFO-6961
+ */
+public class GithubTesting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");    }
+    
+}
